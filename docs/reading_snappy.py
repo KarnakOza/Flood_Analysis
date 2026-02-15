0914a1c680741ed2595798ff73bef09cd0826ec4
Fermy <123935594+KarnakOza@users.noreply.github.com>
2026-02-15 13:21:46 +0530

    Update reading_snappy.py

--- docs/reading_snappy.py
+++ docs/reading_snappy.py
@@ -12,3 +12,19 @@ name = product.getName()
 print("Name: {}".format(name))
 band_names = product.getBandNames()
 print("Band names: {}".format(", ".join(band_names)))
+
+#Product In-line
+
+def plotBand(product, band, vmin, vmax):
+band = product.getBand(band)
+w = band.getRasterWidth()
+h = band.getRasterHeight()
+print(w, h)
+band_data = np.zeros(w * h, np.float32)
+band.readPixels(0, 0, w, h, band_data)
+band_data.shape = h, w
+width = 12
+height = 12
+plt.figure(figsize=(width, height))
+imgplot = plt.imshow(band_data, cmap=plt.cm.binary, vmin=vmin, vmax=vmax)
+return imgplot
